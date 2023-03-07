f5091fd16498487dc23a841ee7d6d4bc6d80d8ea
Arafat <arafats144@gmail.com>
2023-03-07 19:10:23 +0300

    add task

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -10,14 +10,17 @@ class TrustProxies extends Middleware
     /**
      * The trusted proxies for this application.
      *
-     * @var array|string|null
+     * @var string|array
      */
-    protected $proxies;
-
+    protected $proxies = [
+        '192.168.1.1',
+        '192.168.1.2',
+    ];
+ 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO;
 }
