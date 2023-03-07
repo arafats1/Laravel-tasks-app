e2879c3d530d32114bd893f258ff83ae54a98db1
Arafat <arafats144@gmail.com>
2023-03-07 21:46:52 +0300

    change header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,12 +25,12 @@
             @if (Route::has('login'))
                 <div class=" fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underlinetext-gray-900 dark:text-white">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-2000 underline">Login</a>
+                        <a href="{{ route('login') }}" class="text-sm text-gray-2000 underline text-gray-900 dark:text-white">Login</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-2000 underline">Register</a>
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-2000 underline text-gray-900 dark:text-white">Register</a>
                         @endif
                     @endif
                 </div>
@@ -39,7 +39,7 @@
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                <h2 class="text-center text-uppercase text-muted" >My TODO </h2>
+                <h2 class="text-center text-gray-900 dark:text-white" >My To-Do App </h2>
                 
                 </div>
 
@@ -48,7 +48,7 @@
                         <div class="p-6">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Why a TODO list</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Why a to-do list</a></div>
                             </div>
 
                             <div class="ml-12">
